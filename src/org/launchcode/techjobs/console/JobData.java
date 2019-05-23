0235308b6cfeef632cc1b3ed8232bebac153acf8
Lindsey McDanel <lindsey.mcdanel@gmail.com>
2019-05-23 10:45:41 -0500

    added printJobs, case insensitive

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -57,12 +58,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -84,9 +85,33 @@ public class JobData {
         return jobs;
     }
 
+
     /**
      * Read in data from a CSV file and store it in a list
      */
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        value = value.toLowerCase();
+
+        loadData();
+
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+
+            for (HashMap<String, String> job : allJobs) {
+                for(String key : job.keySet()) {
+                    if (job.get(key).toLowerCase().contains(value)) {
+                        list.add(job);
+                        break;
+                    }
+                }
+            }
+
+            return list;
+
+    }
+
+
     private static void loadData() {
 
         // Only load data once
@@ -123,6 +148,7 @@ public class JobData {
             System.out.println("Failed to load job data");
             e.printStackTrace();
         }
-    }
 
+    }
 }
+
